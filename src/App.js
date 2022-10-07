970c583a492ca35a280691f0f0cd48b8a770ced3
fluxit000 <lukasztrusk@gmail.com>
2022-10-07 21:17:02 +0200

    Create nav bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <nav>
+        <div id='shop-title'>
+          Simple Shop
+        </div>
+        <input autoComplete="off" type="text"/>
+        <div className='icons'>
+          <span class="icon-svg">
+            person_outline
+          </span>
+          <span class="icon-svg">
+            shopping_cart
+          </span>
+        </div>
+      </nav> 
+      <main>
+
+      </main>
+    </>
   );
 }
 
